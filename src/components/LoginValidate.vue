16e01411ce4732ad8c67d7b1a0618ef85178d37b
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-13 10:46:07 +0300

    add: Login modal

---
+++ src/components/LoginValidate.vue
@@ -0,0 +1,120 @@
+<template>
+  <modal
+      title="Login"
+      @close="onSubmit()"
+  >
+    <!-- body -->
+    <div slot="body">
+      <form @submit.prevent="onSubmit">
+        <!--E-mail-->
+        <div class="form-item" :class="{errorInput: $v.email.$error}">
+          <label>Email:</label>
+          <p class="errorText" v-if="!$v.email.required">Field is required</p>
+          <p class="errorText" v-if="!$v.email.email">E-mail is not correct !</p>
+          <input v-model="email"
+                 :class="{error: $v.email.$error}"
+                 @change="$v.email.$touch()">
+        </div>
+
+        <!--Password-->
+        <div class="form-item" :class="{errorInput: $v.password.$error}">
+          <label>Password:</label>
+          <p class="errorText" v-if="!$v.password.required">Field is required</p>
+          <p class="errorText" v-if="!$v.password.minLength">Password length must be more than {{ $v.password.$params.minLength.min }} !</p>
+          <input v-model="password"
+                 type="password"
+                 :class="{error: $v.password.$error}"
+                 @change="$v.password.$touch()">
+        </div>
+        <button class="btn btnPrimary">Войти</button>
+        <div class="registry-link">
+          <router-link to="/registration">Мне нужен аккаунт</router-link>
+        </div>
+      </form>
+    </div>
+  </modal>
+</template>
+<script>
+import {required, minLength, email} from 'vuelidate/lib/validators'
+import modal from '@/components/UI/Modal.vue'
+export default {
+  components: {modal},
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$v.$touch()
+      if(!this.$v.$invalid) {
+        const  user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user);
+      }
+      this.email = ''
+      this.$v.$reset()
+      this.password = ''
+      this.$emit('close')
+    },
+  }
+}
+</script>
+<style lang="scss">
+.form-item .errorText {
+  display: none;
+  margin-bottom: 8px;
+  font-size: 13px;
+  color: #DE4040;
+}
+
+.form-item {
+  position: relative;
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+
+  &.errorInput .errorText {
+    display: block;
+    position: absolute;
+    top: 23px;
+    right: 23px;
+  }
+  input {
+    height: auto;
+    padding: 10px;
+    margin: 10px;
+  }
+  label {
+    font-size: 14px;
+  }
+}
+input.error {
+  border-color: #DE4040;
+}
+.registry-link {
+  font-size: 16px;
+  margin-top: 20px;
+  color: #999999;
+  text-decoration: underline;
+  font-weight: bold;
+  cursor: pointer;
+  transition: .3s ease-in-out;
+  &:hover {
+    color: #5247e7;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+<!--        <h1 class="home-title">-->
+<!--          Heavy homework with the implementation of login and registration windows with a switch between them</h1>-->
+      </div>
+      <div class="button-block">
+        <LoginValidate
+            v-show="loginValidate"
+            @close="loginValidate = !loginValidate"
+        />
+        <button v-show="!loginValidate" class="btn btnPrimary" @click="loginValidate = !loginValidate">Логин</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+
+import modal from "@/components/UI/Modal";
+import ModalValidate from "@/components/ModalValidate";
+import LoginValidate from "@/components/LoginValidate";
+
+export default {
+  name: 'Login',
+  components: {
+    LoginValidate,
+    modal
+  },
+  data() {
+    return {
+       loginValidate: true,
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home-title {
+  text-align: center;
+}
+.button-block {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  button {
+    margin-right: 20px;
+  }
+}
+</style>
